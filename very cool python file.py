6a4efe8b1ca60791afb95c7d13560b582fe88211
George Dimas <georgedimas333@gmail.com>
2025-12-04 16:48:05 -0600

    update

--- very cool python file.py
+++ very cool python file.py
@@ -3,8 +3,8 @@ import pika, sys, os, time
 
 rabbitmq_host = 'murabbitmq.ghhsdadqfycscha9.northcentralus.azurecontainer.io'
 rabbitmq_port = "5672"
-rabbitmq_username = 'imchrc'
-rabbitmq_password = 'imchrc'
+rabbitmq_username = 'gadtbg'
+rabbitmq_password = 'gadtbg'
 rabbitmq_vhost = '/'
 
 credentials = pika.PlainCredentials(rabbitmq_username, rabbitmq_password)
@@ -21,12 +21,11 @@ def main():
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.queue_declare(queue='imchrc_STEP0')
     print("running")
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
 
-    channel.basic_consume(queue='imchrc_STEP0', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='gadtbg_STEP0', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
